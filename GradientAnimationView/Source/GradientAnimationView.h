d977c047d8fa1bc606440c6fedd00bfa6a9e9aa8
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-05-03 01:32:05 +0900

    Create framework project

---
+++ GradientAnimationView/Source/GradientAnimationView.h
@@ -0,0 +1,19 @@
+//
+//  GradientAnimationView.h
+//  GradientAnimationView
+//
+//  Created by komaji on 2018/05/03.
+//  Copyright © 2018年 komaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientAnimationView.
+FOUNDATION_EXPORT double GradientAnimationViewVersionNumber;
+
+//! Project version string for GradientAnimationView.
+FOUNDATION_EXPORT const unsigned char GradientAnimationViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientAnimationView/PublicHeader.h>
+
+
